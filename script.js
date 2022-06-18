d8bf75eea385035a5a2abf3f3a6cdcb282df56cd
AgustinForns <103077691+AgustinForns@users.noreply.github.com>
2022-06-18 12:08:46 -0300

    1 preentrega

--- script.js
+++ script.js
@@ -1,55 +1,194 @@
 
 
-const personaje = [
-    {nombre: "rachel", apellido:"green"},
-    {nombre: "ross", apellido:"geller"},
-    {nombre: "phoebe", apellido:"buffay"},
-    {nombre: "chandler", apellido:"bing" },
-    {nombre: "monica", apellido:"geller" },
-    {nombre: "joey", apellido:"tribbiani"},   
+
+const datosPersonajes = [
+    {id: 1, nombre: "rachel", apellido:"green"},
+    {id: 2, nombre: "ross", apellido:"geller"},
+    {id: 3, nombre: "phoebe", apellido:"buffay"},
+    {id: 4, nombre: "chandler", apellido:"bing" },
+    {id: 5, nombre: "monica", apellido:"geller" },
+    {id: 6, nombre: "joey", apellido:"tribbiani"},   
 ];
 
-
-class InicioJuego{
-    constructor()
+const inicioJuego =() =>{ 
+    segundoJuego(primerJuego());
 }
 
 
+const primerJuego = () => {
+    alert("Te diremos el nombre o apellido de un personaje y tienes que introducir el dato que no te demos. Si tienes 3 respuestas correctas pasas a la asgeunda sección del juego");
+    let correctas = 0;
+    let incorrectas = 0;
+    datosPersonajes.forEach(element => {
+        const indexNombreApellido = Math.round(Math.random() * 1);
+        if (indexNombreApellido === 0) {
+             if (saberNombre(element) === true) {
+                correctas++;
+            } else {
+                incorrectas++;
+            }
+        }
+        else{
+            if (saberApellido(element) === true) {
+                correctas++;
+            } else {
+                incorrectas++;
+            }
+        }
+      
+    });
+
+    let resultado = resumenJuego(correctas, incorrectas);
+    return resultado;
+}
+
+const resumenJuego = (corr, incorr)=> {
+    alert(`Como resultado obtuviste ${corr} respuestas correctas y ${incorr} respuestas incorrectas`);
+    if (corr > 2) {
+        return true
+    } else {
+        return false;
+    }
+}
 
 
+const saberNombre = (persona) =>{
+    let respuesta = true; 
+    let nombreIntroducido = prompt(`El nombre del personaje que se apellida ${persona.apellido} es:`);
+    if (nombreIntroducido.toLowerCase() === persona.nombre ) {
+        alert(`Correcto. El personaje es: ${persona.nombre} ${persona.apellido}`);
+        respuesta = true;
+    } else {
+        alert(`Incorrecto. El personaje es: ${persona.nombre} ${persona.apellido}`);
+        respuesta = false;
+    }
+    return respuesta;
+}
+
+const saberApellido = (persona) =>{
+    let respuesta = true;  
+    let nombreIntroducido = prompt(`El apellido del personaje que tiene como nombre ${persona.nombre} es:`);
+    if (nombreIntroducido.toLowerCase() === persona.apellido ) {
+        alert(`Correcto. El personaje es: ${persona.nombre} ${persona.apellido}`);
+        respuesta = true;
+    } else {
+        alert(`Incorrecto. El personaje es: ${persona.nombre} ${persona.apellido}`);
+        respuesta = false;
+    }
+    return respuesta;
+}
+
+const mostrarPersonajes = () => {
+    alert(` La lista de personajes es:
+    ${datosPersonajes[0].id} ${datosPersonajes[0].nombre} ${datosPersonajes[0].apellido}
+    ${datosPersonajes[1].id} ${datosPersonajes[1].nombre} ${datosPersonajes[1].apellido}
+    ${datosPersonajes[2].id} ${datosPersonajes[2].nombre} ${datosPersonajes[2].apellido}
+    ${datosPersonajes[3].id} ${datosPersonajes[3].nombre} ${datosPersonajes[3].apellido}
+    ${datosPersonajes[4].id} ${datosPersonajes[4].nombre} ${datosPersonajes[4].apellido}
+    ${datosPersonajes[5].id} ${datosPersonajes[5].nombre} ${datosPersonajes[5].apellido}
+    
+    `)
+
+}
 
 
-class Personajes{
-
-    constructor(personaje){
-        this.nombre = personaje.nombre;
-        this.apellido = personaje.apellido;
+const segundoJuego = (resultado) => {
+    if (resultado === true) {
+        let nom = prompt("Vamos a ver si te acuerdas lo anterior. Tienes que dar un nombre y apellido sin equivocarte. Primero da un nombre: ");
+        if ((datosPersonajes.some(id => id.nombre === nom.toLowerCase())) === true) {
+            let apell = prompt(`Es correcto! Un personaje se llama ${nom}. Nos puedes decir su apellido?`);
+            if ((datosPersonajes.find((id) => id.nombre === nom)).apellido === apell) {
+                alert(`Es correcto! El nombre y apellido del personaje es ${nom} ${apell}`);
+                mostrarPersonajes();
+                alert("Ganaste la segunda parte del juego!!")
+            } else {
+                alert(`Incorrecto. El nombre y apellido del personaje es ${nom} ${(datosPersonajes.find(id => id.nombre === nom)).apellido}`);
+                mostrarPersonajes();
+                alert("El juego termino!!!")
+            }
+        } else {     
+            alert(`Incorrecto. El nombre que ingresaste no pertenece a ningun personaje`);
+            mostrarPersonajes();
+            alert("El juego termino!!!")
+        }   
+    } else {
+        alert("Nos vemos la proxima!")
     }
 
-    preguntas = [saberNombre, saberApellido];
 
-    saberNombre(){
-        let respuesta = true;
-        alert("Te diremos el apellido de un personaje y tiene que introducir su nombre");
-        const indexNombre = Math.round(Math.random() * 5);
-       nombreIntroducido = prompt(`El nombre del personaje que se apellida ${personaje.apellido[indexNombre]} es:`);
-        if (nombreIntroducido === personaje.nombre[indexNombre] ) {
-            alert(`Correcto. El personaje es: ${personaje.nombre} ${personaje.apellido}`);
-            respuesta = true;
-        } else {
-            alert(`Correcto. El personaje es: ${personaje.nombre} ${personaje.apellido}`);
-            respuesta = false;
+
+
+    
+}
+
+
+inicioJuego()  
+ 
+
+
+
+
+
+
+
+
+
+/* const inicioJuego = () => {
+    alert("Bienvenido a QuienSabeMasFriends");
+    let 
+
+    const indexNombreApellido = Math.round(Math.random() * 1);
+    const indexPersonaje = Math.round(Math.random() * (personaje.length));
+
+    const nuevoJuego = new PreguntasPersonales(datosPersonajes[indexPersonaje]);
+    nuevoJuego.preguntas[indexNombreApellido];
+
+
+    let decision = "s"
+    while ((decision !== "N") && (decision !== "n") ) {
+        let correctas = 0
+        let incorrectas = 0
+        let cantidad = cantidadPreg();
+        for (let pregunta = 1; pregunta <= cantidad; pregunta++) {
+            let respuesta = prompt("Tu respuesta es correcta?")
+            if ((respuesta == "True")) {
+                correctas++
+            } else {
+                incorrectas++
+            }
         }
+        if (cantidad !== false) {
+            resumenJuego(correctas, incorrectas, cantidad)
+        } else {
+            alert("Introduce un numero menor 5")
+        }
+
         
     }
 
+    alert("Gracias por jugar!!")
 
-    eleccion(){
-        const indexNombreApellido = Math.round(Math.random() * 2);
-        preguntas[indexNombreApellido];
-    }
-    
+
 }
+
+
+const contar = (result) => {
+    let correctas = 0;
+    let incorrectas = 0;
+    if (result === True) {
+        correctas++
+    } else {
+        incorrectas++
+    }
+    return correctas, incorrectas;
+}
+
+
+
+
+
+
+
 
 
 
@@ -79,7 +218,7 @@ const cantidadPreg = ()=>{
         
     }
 }
-
+ 
 
 const resumenJuego = (corr, incorr, cantidad)=> {
     if (corr > (cantidad/2)) {
@@ -91,31 +230,4 @@ const resumenJuego = (corr, incorr, cantidad)=> {
     }
     
 }
-
-
-
-
-alert(`Responde con un True y tu respuesta será correcta. Si tienes mas de la mitad correctas ganas el juego. Tienes tres chances (Te diremos al final el resultado)`)
-let decision = "s"
-while ((decision !== "N") && (decision !== "n") ) {
-    let correctas = 0
-    let incorrectas = 0
-    let cantidad = cantidadPreg();
-    for (let pregunta = 1; pregunta <= cantidad; pregunta++) {
-        let respuesta = prompt("Tu respuesta es correcta?")
-        if ((respuesta == "True")) {
-            correctas++
-        } else {
-            incorrectas++
-        }
-    }
-    if (cantidad !== false) {
-        resumenJuego(correctas, incorrectas, cantidad)
-    } else {
-        alert("Introduce un numero menor 5")
-    }
-
-    
-}
-
-alert("Gracias por jugar!!")
+ */
